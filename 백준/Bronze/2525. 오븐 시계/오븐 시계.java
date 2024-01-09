fb04e487affd61c18883bffc871f1f18ab770799
hazzokko <134523731+hazzokko@users.noreply.github.com>
2024-01-09 17:03:35 +0900

    [Bronze III] Title: 오븐 시계, Time: 224 ms, Memory: 18524 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -8,16 +8,9 @@ public class Main {
         int B = sc.nextInt();
         int C = sc.nextInt();
 
-        if ((B + C) < 60) {
-            System.out.println(A + " " + (B + C));
-        } else {
-            int plusHour = A + ((B + C) / 60);
-            int min = (B + C) % 60;
-            if (plusHour >= 24) {
-                System.out.println(plusHour - 24 + " " + min);
-            } else {
-                System.out.println(plusHour + " " + min);
-            }
-        }
+        int hour = (A + ((B + C) / 60)) % 24;
+        int min = (B + C) % 60;
+
+        System.out.println(hour + " " + min);
     }
 }
