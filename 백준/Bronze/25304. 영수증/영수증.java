4906219c97879ce67fb5a92adec7657d37cd4a87
hazzokko <134523731+hazzokko@users.noreply.github.com>
2024-01-14 16:03:34 +0900

    [Bronze IV] Title: 영수증, Time: 224 ms, Memory: 17832 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int total = sc.nextInt();
+        int category = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 0; i < category; i++) {
+            int price = sc.nextInt();
+            int count = sc.nextInt();
+            sum += price * count;
+        }
+
+        if (total == sum) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
