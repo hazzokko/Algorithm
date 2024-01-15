dfb4330690c00408bff60a82fcab969f0284bb51
hazzokko <134523731+hazzokko@users.noreply.github.com>
2024-01-15 23:57:20 +0900

    [Bronze V] Title: A+B - 7, Time: 252 ms, Memory: 18772 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int total = sc.nextInt();
+        for (int i = 0; i < total; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println("Case #" + (i + 1) + ": " + (a + b));
+        }
+    }
+}
