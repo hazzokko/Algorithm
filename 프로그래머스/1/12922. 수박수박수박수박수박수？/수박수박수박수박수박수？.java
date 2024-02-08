6cb37b40043f585dbb4ad6701766dff3f6f04459
hazzokko <134523731+hazzokko@users.noreply.github.com>
2024-02-09 00:35:14 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.95 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "수";
+        for (int i = 2; i <= n; i++) {
+            if (i % 2 == 0) {
+                answer += "박";
+            } else {
+                answer += "수";
+            }
+        }
+        return answer;
+    }
+}
