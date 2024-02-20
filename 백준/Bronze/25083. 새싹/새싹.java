bb7a7836217c6b7cb4f3f0f7cfab662de84d44fa
hazzokko <134523731+hazzokko@users.noreply.github.com>
2024-02-21 00:23:33 +0900

    [Bronze V] Title: 새싹, Time: 112 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,10 @@
+interface Main {
+    static void main(String[] s) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
