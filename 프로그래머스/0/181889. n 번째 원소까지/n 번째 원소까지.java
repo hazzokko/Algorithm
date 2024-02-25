3009b6c8df069a4708274b69b4f8cfe5ac084f36
hazzokko <134523731+hazzokko@users.noreply.github.com>
2024-02-25 16:01:55 +0900

    [level 0] Title: n 번째 원소까지, Time: 2.10 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = Arrays.stream(num_list).limit(n).toArray();
+        return answer;
+    }
+}
