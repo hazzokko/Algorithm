75b426f314468e9ecd341887fa3c5a097b4d4ed9
hazzokko <134523731+hazzokko@users.noreply.github.com>
2024-03-13 13:29:30 +0900

    [Bronze V] Title: 직사각형, Time: 204 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,7 @@
+interface Main {
+    static void main(String[] s) {
+        var v = new java.util.Scanner(System.in);
+        int A = v.nextInt(), B = v.nextInt();
+        System.out.println(A * B);
+    }
+}
