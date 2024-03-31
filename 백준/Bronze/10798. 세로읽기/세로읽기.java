5301116f761bf8469cdbd6885982feaffa91c194
hazzokko <134523731+hazzokko@users.noreply.github.com>
2024-03-31 13:07:37 +0900

    [Bronze I] Title: 세로읽기, Time: 232 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,26 @@
+interface Main {
+    static void main(String[] str) {
+        var v = new java.util.Scanner(System.in);
+        String[][] arr = new String[5][15];
+        String result = "";
+
+        for (int i = 0; i < 5; i++) {
+            String s = v.nextLine();
+            for (int j = 0; j < s.length(); j++) {
+                arr[i][j] = String.valueOf(s.charAt(j));
+            }
+        }
+
+        for (int j = 0; j < 15; j++) {
+            for (int i = 0; i < 5; i++) {
+                if (arr[i][j] != null) {
+                    result += arr[i][j];
+                } else {
+                    result += "";
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
