aae03b0856f780499ffc2dc368f78d9393e625d0
hazzokko <134523731+hazzokko@users.noreply.github.com>
2024-05-02 17:20:23 +0900

    [level 0] Title: 배열 비교하기, Time: 0.76 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        int sum1 = Arrays.stream(arr1).sum();
+        int sum2 = Arrays.stream(arr2).sum();
+        
+        if (arr1.length != arr2.length) {
+            if (arr1.length < arr2.length) {
+                return -1;
+            } else if (arr1.length > arr2.length) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            if (sum1 < sum2) {
+                return -1;
+            } else if (sum1 > sum2) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+}
