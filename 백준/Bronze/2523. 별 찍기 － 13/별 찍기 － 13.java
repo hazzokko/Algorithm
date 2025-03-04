31270004c4def071c4a58d741ae4d821c6b72ba0
hazzokko <134523731+hazzokko@users.noreply.github.com>
2025-03-04 18:49:16 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 280 ms, Memory: 19220 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        for (int i = 1; i <= N; i++) {
+            for (int j = N - i; j < N; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        for (int i = N - 1; i > 0; i--) {
+            for (int j = i; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
