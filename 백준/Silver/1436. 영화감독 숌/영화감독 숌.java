49d4ef62c103f59f023fdf925acbe9b4caafc9d8
hazzokko <134523731+hazzokko@users.noreply.github.com>
2025-03-05 20:35:48 +0900

    [Silver V] Title: 영화감독 숌, Time: 336 ms, Memory: 88956 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.close();
+		int count = 0;
+		int num = 666;
+		
+		while (count < N) {
+			if (String.valueOf(num).contains("666")) {
+				count++;
+				if (count == N) {
+					System.out.println(num);
+				}
+			}
+			num++;
+		}
+	}
+}
