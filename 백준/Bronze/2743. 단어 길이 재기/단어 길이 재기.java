c7bb5dada572647355ab082f25a5de6412fe5da1
hazzokko <134523731+hazzokko@users.noreply.github.com>
2025-06-14 12:52:16 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 172 ms, Memory: 17616 KB -BaekjoonHub

--- 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -1,6 +1,9 @@
-interface Main {
-    static void main(String[] s) {
-        var v = new java.util.Scanner(System.in);
-        System.out.println(v.nextLine().chars().count());
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.print(str.length());
     }
 }
