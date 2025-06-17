561aca8408cd62cf9c8cb0ee2e1a25d262b6aa33
hazzokko <134523731+hazzokko@users.noreply.github.com>
2025-06-17 20:31:26 +0900

    [Bronze V] Title: AI 선도대학, Time: 180 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        sc.close();
+        System.out.print(num - 2024);
+    }
+}
