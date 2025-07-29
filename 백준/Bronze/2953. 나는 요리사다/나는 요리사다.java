b948a873ba9c4d52515a1e8434317b30ff4aef4b
hazzokko <134523731+hazzokko@users.noreply.github.com>
2025-07-29 22:40:59 +0900

    [Bronze III] Title: 나는 요리사다, Time: 112 ms, Memory: 15924 KB -BaekjoonHub

---
+++ 백준/Bronze/2953. 나는 요리사다/나는 요리사다.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int winner = 1;
+        int total = 0;
+        
+        for (int i = 1; i <= 5; i++) {
+            st = new StringTokenizer(br.readLine());
+            int current = 0;
+            while (st.hasMoreTokens()) {
+                current += Integer.parseInt(st.nextToken());
+            }
+            if (current > total) {
+                winner = i;
+                total = current;
+            }
+        }
+        System.out.print(winner + " " + total);
+    }
+}
